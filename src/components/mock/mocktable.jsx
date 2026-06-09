1cd60763718f49f4d9a9dd65141133e86601e1a9
hassan970mail-afk <hassan970mail@gmail.com>
2026-06-09 22:28:08 +0500

    mock page changing

--- src/components/mock/mocktable.jsx
+++ src/components/mock/mocktable.jsx
@@ -5,64 +5,45 @@ import { Link } from "react-router-dom";
 const MockTable = ({ examCode }) => {
   return (
     <div className="mock-table-wrapper">
-
       <table className="mock-table">
-
         <thead>
-
           <tr>
-
             <th>Verbal</th>
 
             <th>Non-Verbal</th>
 
             <th>Academics</th>
-
           </tr>
-
         </thead>
 
         <tbody>
-
           <tr>
-
             <td>
-              <Link
-                to= "/mock/verbal/set1"
-                className="mock-table-btn"
-              >
+              <Link to="/mock/verbal/set1" className="mock-table-btn">
                 Verbal Mock 1
               </Link>
             </td>
 
             <td>
-              <Link
-                to="/mock/nonverbal/set1"
-                className="mock-table-btn"
-              >
+              <Link to="/mock/nonverbal/set1" className="mock-table-btn">
                 Non-Verbal Mock 1
               </Link>
             </td>
 
             <td>
-              <Link
-                to="/mock/academic/set1"
-                className="mock-table-btn"
-              >
+              <Link to="/mock/academic/set1" className="mock-table-btn">
                 Academic Mock 1
               </Link>
             </td>
-
           </tr>
 
           <tr>
-
             <td>
               <Link
                 to={`/mock/${examCode}/verbal-2`}
                 className="mock-table-btn"
               >
-                Verbal Mock 2
+                Not Uploaded
               </Link>
             </td>
 
@@ -71,29 +52,24 @@ const MockTable = ({ examCode }) => {
                 to={`/mock/${examCode}/nonverbal-2`}
                 className="mock-table-btn"
               >
-                Non-Verbal Mock 2
+                Not Uploaded
               </Link>
             </td>
 
             <td>
-              <Link
-                to="/mock/academic/set2"
-                className="mock-table-btn"
-              >
+              <Link to="/mock/academic/set2" className="mock-table-btn">
                 Academic Mock 2
               </Link>
             </td>
-
           </tr>
 
           <tr>
-
             <td>
               <Link
                 to={`/mock/${examCode}/verbal-3`}
                 className="mock-table-btn"
               >
-                Verbal Mock 3
+                Not Uploaded
               </Link>
             </td>
 
@@ -102,25 +78,18 @@ const MockTable = ({ examCode }) => {
                 to={`/mock/${examCode}/nonverbal-3`}
                 className="mock-table-btn"
               >
-                Non-Verbal Mock 3
+                Not Uploaded
               </Link>
             </td>
 
             <td>
-              <Link
-                to="/mock/academic/set3"
-                className="mock-table-btn"
-              >
+              <Link to="/mock/academic/set3" className="mock-table-btn">
                 Academic Mock 3
               </Link>
             </td>
-
           </tr>
-
         </tbody>
-
       </table>
-
     </div>
   );
 };
