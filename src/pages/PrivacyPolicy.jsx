bafbeb6821a036e240773660c15b04f17355f150
hassan970mail-afk <hassan970mail@gmail.com>
2026-06-09 13:18:40 +0500

    privacy policy page added

--- src/pages/PrivacyPolicy.jsx
+++ src/pages/PrivacyPolicy.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import { Helmet } from "react-helmet-async";
 
-import "../../styles/privacy-policy.css";
+import "../styles/privacy-policy.css";
 
 const PrivacyPolicy = () => {
   return (
