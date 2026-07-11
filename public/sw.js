5ae8f8e576ebaacdcfd59e71f91fddeaa1c6e46c
hassan970mail-afk <hassan970mail@gmail.com>
2026-07-11 11:19:02 +0500

    monetag file added----

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,6 @@
-importScripts("https://cdn.pushalert.co/sw-90087.js");
+self.options = {
+    "domain": "3nbf4.com",
+    "zoneId": 11268843
+}
+self.lary = ""
+importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
