12345fdb3bf475113bf634b265e788b869892678
hassan970mail-afk <hassan970mail@gmail.com>
2026-08-28 12:31:43 +0500

    security enabled

--- src/App.jsx
+++ src/App.jsx
@@ -58,8 +58,8 @@ function App() {
 
   return (
     <>
-      {/* <SecurityToast />  */}
-      {/* <SecurityProtection /> */}
+       <SecurityToast />  
+       <SecurityProtection /> 
 
       <ScrollToTop />
       <Routes>
