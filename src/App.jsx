12561dc1163f5e3a6cfeb077473e62abb3dc7500
EXAMITICS <examitics@gmail.com>
2026-09-25 19:52:05 +0500

    Remove incomplete verbal mock set 6 route

--- src/App.jsx
+++ src/App.jsx
@@ -330,19 +330,6 @@ PREMIUM PMA VERBAL MOCK TEST 4
           }
         />
         {/* ==========================================
-    PREMIUM PMA VERBAL MOCK TEST 6
-========================================== */}
-
-        <Route
-          path="/mock/verbal/set6"
-          element={
-            <PremiumRoute>
-              <MockTest exam="pma-lc" category="verbal" setId="set6" />
-            </PremiumRoute>
-          }
-        />
-
-        {/* ==========================================
     PREMIUM PMA NON-VERBAL MOCK TEST 2
 ========================================== */}
 
